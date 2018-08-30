9a93f1ca72740a5771202eca32740511f0783843
hariharan31998 <36722566+hariharan31998@users.noreply.github.com>
2018-08-30 14:51:00 +0530

    Create inv_right_triangle.c

---
+++ player level/inv_right_triangle.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main() {
+  int a;
+  scanf("%d",&a);
+  for (int i=a;i>0;i--)
+  {
+
+      for (int j=i;j>0;j--)
+      {
+          printf("1 ");
+          
+      }
+      printf("\n");
+  }
+}
